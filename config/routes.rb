5cbbc5c9e5e45c1a2b9d7340b6c70784cf000f86
NamNguyen911 <mr.nam911@gmail.com>
2017-08-01 23:09:17 +0700

    set mount point for shopify engine

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   root :to => 'home#index'
   post 'selectShop', to: 'home#selectShop'
-  mount ShopifyApp::Engine, at: '/'
+  mount ShopifyApp::Engine, at: '/shopify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
