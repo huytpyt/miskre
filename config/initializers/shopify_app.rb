65723d9cfa44d8a7c4012bd1bb7c914df37b1c57
NamNguyen911 <mr.nam911@gmail.com>
2017-08-01 07:48:44 +0700

    disable embedded app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,5 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "db4490d18b89d1a5b54af8f9fff9df9b"
   config.secret = "856da6a99df9e70eeb23a7cf8029eb97"
   config.scope = "read_orders, read_products"
-  config.embedded_app = true
+  # config.embedded_app = true
+  config.embedded_app = false
 end
