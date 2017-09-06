762be6251a72a9bb8ec917ab3f8743a031772cd6
NamNguyen911 <mr.nam911@gmail.com>
2017-09-06 11:12:40 +0700

    view and filter orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,16 +10,25 @@ class OrdersController < ApplicationController
     end
   end
 
-  private 
-
+  private
   def set_query
     @start_date = params[:start_date] || Date.current - 7
     @end_date = params[:end_date] || Date.current
+    @financial_status = params[:financial_status].to_s
+    @fulfillment_status = params[:fulfillment_status].to_s
+
+    query_params = {}
+    if @fulfillment_status == "null"
+      query_params['fulfillment_status'] = nil
+    else
+      query_params['fulfillment_status'] = @fulfillment_status unless @fulfillment_status.empty?
+    end
+    query_params['financial_status'] = @financial_status unless @financial_status.empty?
 
     if params[:shop_id]
       begin
         @current_shop = Shop.find(params[:shop_id])
-        @orders = @current_shop.orders.where(date: @start_date..@end_date)
+        @orders = @current_shop.orders.where(date: @start_date..@end_date).where(query_params)
       rescue ActiveRecord::RecordNotFound
         @current_shop = nil
         @orders = []
@@ -27,7 +36,7 @@ class OrdersController < ApplicationController
     else
       unless current_user.shops.empty?
         @current_shop = current_user.shops.first
-        @orders = @current_shop.orders.where(date: @start_date..@end_date)
+        @orders = @current_shop.orders.where(date: @start_date..@end_date).where(query_params)
       else
         @current_shop = nil
         @orders = []
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     @shops = current_user.shops.select(:id, :name).all
     @shops.collect {|s| [s.name, s.id]}
   end
+
+  def financial_options
+    [['Pending', 'pending'], ['Authorized', 'authorized'],
+     ['Partially Paid', 'partially_paid'], ['Paid', 'paid'],
+     ['Partially Refunded', 'partially_refunded'],
+     ['Refunded', 'refunded'], ['Voided', 'voided']]
+  end
+
+  def fulfillment_options
+    [['Unfulfilled', 'null'], ['Fulfilled', 'fulfilled'],
+     ['Partially fulfilled', 'partial']]
+  end
 end
