eb6ac9e951ff0dafef59410510a66ba894ee89dd
Huy Tran <huytp.uit@gmail.com>
2017-09-18 18:08:40 +0700

    Fix Fulfillment sync

--- app/jobs/fulfillments_sync_job.rb
+++ app/jobs/fulfillments_sync_job.rb
@@ -4,8 +4,8 @@ class FulfillmentsSyncJob < ApplicationJob
   def perform(billing_id)
     Shop.all.each do |shop|
       p shop.name
-      communicator = ShopifyCommunicator.new(shop.id, billing_id)
-      communicator.sync_fulfillments(shop)
+      communicator = ShopifyCommunicator.new(shop.id)
+      communicator.sync_fulfillments(billing_id)
     end
   end
 end
