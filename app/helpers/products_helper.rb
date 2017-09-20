0771bf8ca89cf1c5d8e569524cc02bf0f2f5c888
Huy Tran <huytp.uit@gmail.com>
2017-09-20 11:10:26 +0700

    Change name for variant

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def variant_name product_name, variant
+    "#{product_name}#{variant.option1.present? ? ' - ' + variant.option1 : ''}#{variant.option2.present? ? ' - ' + variant.option2 : ''}#{variant.option3.present? ? ' - ' + variant.option3 : ''}"
+  end
 end
