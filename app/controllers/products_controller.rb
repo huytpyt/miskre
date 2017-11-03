9ee1240a87c31e20a03e3f712571e9a96965866a
Huy Tran <huytp.uit@gmail.com>
2017-11-03 22:37:41 +0700

    fix products list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
   def index
     staff_ids = User.where.not(role: "user").ids
     # @products = Product.order(sku: :asc).page params[:page]
-    @products = Product.all.where(shop_owner: false, user_id: staff_ids)
+    @products = Product.all.where(shop_owner: false, user_id: [staff_ids, nil])
     @request_products = current_user.request_products
     @my_products = current_user.products.where(shop_owner: false)
     respond_to do |format|
