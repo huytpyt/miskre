a6d1b9d1c574bfcad4f0705f5653e000b715ec2c
Huy Tran <huytp.uit@gmail.com>
2017-11-08 22:11:20 +0700

    Fix carrier

--- app/services/carrier_service.rb
+++ app/services/carrier_service.rb
@@ -4,6 +4,7 @@ class CarrierService
     unless user_nation.present?
       return []
     end
+
     user_shipping_types = user_nation.user_shipping_types.where(active: true)
     rates = []
     user_shipping_types.each do |user_shipping_type|
@@ -11,15 +12,15 @@ class CarrierService
       user_shipping_settings = user_shipping_type.shipping_settings.where("min_price < ?", total_price).last
       if shipping_type.has_handling
         detail = shipping_type.detail_shipping_types.where("weight_from <= ? AND ? <= weight_to", weight, weight).last
-        total_original_cost = ((detail.cost * (weight.to_f/1000) + detail.handling_fee + 0.5)* 100).round(0).to_s if detail.present?
+        total_original_cost = ((detail.cost * (weight.to_f/1000) + detail.handling_fee + 0.5)* 100).round(0) if detail.present?
       else
         detail = shipping_type.detail_no_handlings.where("weight_from <= ? AND ? <= weight_to", weight, weight).last
-        total_original_cost = ((detail.cost  + 0.5) * 100).round(0).to_s if detail.present?
+        total_original_cost = ((detail.cost  + 0.5) * 100).round(0) if detail.present?
       end
       beus_type = Nation.find_by_code("US").shipping_types.find_by_code("BEUS")
       beus_cost = cal_cost(beus_type, weight)
       diff_cost = total_original_cost  != beus_cost ? (total_original_cost - beus_cost)*shop.shipping_rate : 0
-      shipping_price = ((1 - shop.shipping_rate)*total_original_cost + diff_cost).round(2)
+      shipping_price = ((1 - shop.shipping_rate)*total_original_cost + diff_cost).round(2).to_s
       if detail.present?
         item = {'service_name': user_shipping_settings.packet_name, 'service_code': shipping_type.code, 'currency': 'USD', 'total_price': shipping_price} 
         rates.push(item)
