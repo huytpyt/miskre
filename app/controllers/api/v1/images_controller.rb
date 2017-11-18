5a1c38e48bb635586939a2c824f3a8dece3fcad9
Quan Nguyen <quanmn.libra@gmail.com>
2017-11-19 00:44:43 +0700

    Added images API

---
+++ app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::ImagesController < Api::V1::BaseController
+	before_action :validate_image_base64, only: :create
+	before_action :prepare_image, only: [:show, :destroy]
+
+	def index
+		page = params[:page].to_i || 1
+    page = 1 if page.zero?
+    per_page = params[:per_page].to_i || 20
+    per_page = 20 if per_page.zero?
+    total_page = Image.count / per_page
+    total_page = total_page <= 0 ? 1 : total_page
+    sort = params[:sort] || 'DESC'
+    order_by = params[:order_by] || 'id'
+    render json: ImagesQuery.list(page, per_page, sort, order_by), status: 200
+  end
+
+  def create
+  	image = Image.new
+    image.set_image_base64(params[:image])
+    if image.save
+    	render :json => { status: true, image: ImagesQuery.single(image)}, status: 200
+    else
+    	render json: {status: false, error: "Can not upload this image!"}, status: 500
+    end
+  end
+
+  def show
+    render :json => { status: true, image: ImagesQuery.single(@image) }, status: 200
+  end
+
+  def destroy
+    @image.destroy
+    head :no_content
+  end
+
+  private
+
+  	def validate_image_base64
+  		if params[:image].present?
+		    # rexp = /data:image\/(?:gif|jpg|jpeg|tiff|png);base64,(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)/
+		    # if rexp.match(params[:image])
+		    # 	true
+		    # else
+		    #   render json: {status: false, error: "Image must base64 format"}, status: 500
+		    # end
+		    true
+		  else
+		  	render json: {status: false, error: "Image can't be blank"}, status: 500
+		  end
+	  end
+
+	  def prepare_image
+	  	@image = Image.find(params[:id])
+	  rescue
+	  	render json: {status: false, error: "Not found!"}, status: 404
+	  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -164,6 +164,7 @@ Rails.application.routes.draw do
         end
       end
       resources :products
+      resources :images
     end
   end
 
