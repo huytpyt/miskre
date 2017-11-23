c4529f251125aebc4e3cd90acfe4479298802269
Quan Nguyen <quanmn.libra@gmail.com>
2017-11-23 09:06:14 +0700

    Update variants with images

--- app/controllers/api/v1/variants_controller.rb
+++ app/controllers/api/v1/variants_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::VariantsController < Api::V1::BaseController
 
   # GET /api/v1/products/:product_id/variants
   def index
-    render :json => {status: true, variants: @product.variants}, status: 200
+    render :json => {status: true, variants: VariantsQuery.list(@product)}, status: 200
   end
 
   # POST /api/v1/products/:product_id/variants/reload
   def reload
     @product.regen_variants
-    render :json => {status: true, variants: @product.variants}, status: 200
+    render :json => {status: true, variants: VariantsQuery.list(@product)}, status: 200
   end
 
   # POST /api/v1/products/:product_id/variants
@@ -19,21 +19,37 @@ class Api::V1::VariantsController < Api::V1::BaseController
 
   # GET /api/v1/products/:product_id/variants/:id
   def show
-  	render :json => {status: true, variants: @variant}, status: 200
+  	render :json => {status: true, variant: VariantsQuery.single(@variant)}, status: 200
   end
 
   # PUT /api/v1/products/:product_id/variants/:id
   # {
   # 	"price": number,
   # 	"quantity": integer,
-  # 	"sku": string
+  #   "option1": string,
+  #   "option2": string,
+  #   "option3": string,
+  #   "images": [image_id_1, image_id_2]
   # }
   def update
-  	if @variant.update(variant_params)
-  		render :json => {status: true, variants: @variant}, status: 200
-  	else
-  		render json: {status: false, :errors => @variant.errors.messages }, status: 500
-  	end
+    if params[:variant].present?
+    	if @variant.update(variant_params)
+        if params[:variant][:images].present?
+          if params[:variant][:images].is_a?(Array)
+            exists_ids = params[:variant][:images].select{|id| Image.exists?(id)}
+            @variant.image_ids = exists_ids
+            @variant.save
+          else
+            render json: {status: false, error: "`images` must an array"}, status: 500
+          end
+        end
+    		render :json => {status: true, variant: VariantsQuery.single(@variant)}, status: 200
+    	else
+    		render json: {status: false, :errors => @variant.errors.messages }, status: 500
+    	end
+    else
+      render :json => {status: true, variant: VariantsQuery.single(@variant)}, status: 200
+    end
   end
 
   # DELETE /api/v1/products/:product_id/variants/:id
@@ -45,6 +61,6 @@ class Api::V1::VariantsController < Api::V1::BaseController
   private
 
   	def variant_params
-    params.require(:variant).permit(:quantity, :price, :sku)
-  end
+      params.require(:variant).permit(:option1, :option2, :option3, :quantity, :price, :user_id, :compare_at_price, :product_ids)
+    end
 end
---
+++ app/queries/variants_query.rb
@@ -0,0 +1,40 @@
+class VariantsQuery < BaseQuery
+
+	def self.list(product)
+		product.variants.map{ |variant| single(variant) }
+	end
+
+	def self.single(variant)
+		{
+			id: variant.id,
+			option1: variant.option1,
+			option2: variant.option2,
+			option3: variant.option3,
+			quantity: variant.quantity,
+			price: variant.price,
+			sku: variant.sku,
+			product_id: variant.product_id,
+			user_id: variant.user_id,
+			compare_at_price: variant.compare_at_price,
+			product_ids: variant.product_ids,
+			images: images_for(variant),
+			created_at: variant.created_at,
+			updated_at: variant.updated_at
+		}
+	end
+
+	private
+
+		def self.images_for(variant)
+			variant.images.map do |image|
+				{
+					id: image.id,
+					url: image.file_url,
+		  		thumb: image.file.url(:thumb),
+		  		medium: image.file.url(:medium)
+				}
+			end
+		end
+
+end
+
