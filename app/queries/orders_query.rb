c620d7f5400c5bfa72d48c05c1597718cdb41c34
thoaivk <thoaivk@miskre.com>
2018-01-04 17:04:07 +0700

    Update order query

--- app/queries/orders_query.rb
+++ app/queries/orders_query.rb
@@ -83,7 +83,6 @@ class OrdersQuery < BaseQuery
     status, errors, top_20_product, duration = shop_data
     {
       duration: duration,
-      total_orders: Order.where("created_at > :duration", duration: duration.days.ago.end_of_day).count,
       product_ranking: top_20_product.map{|product| single_ranking(product)}
     }
   end
@@ -99,7 +98,6 @@ class OrdersQuery < BaseQuery
         shop_id: shop.id,
         shop_name: shop.name,
         duration: duration,
-        total_orders: Order.where("shop_id = :shop_id AND created_at > :duration", { duration: duration.days.ago.end_of_day, shop_id: shop.id }).count,
         shop_statistics: shop_statistics.map{|product| single_ranking(product)}
       }
     end
