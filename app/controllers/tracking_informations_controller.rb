960df451c5a54376c23ced44ecc6c34ab0cd7d67
ThoaiVo <vokythoai@gmail.com>
2018-01-25 17:55:22 +0700

    Update tracking information page

--- app/controllers/tracking_informations_controller.rb
+++ app/controllers/tracking_informations_controller.rb
@@ -2,7 +2,7 @@ class TrackingInformationsController < ActionController::Base
   def show
     @order = Order.find_by_shopify_id params[:id]
     token = params[:token]
-    if token && @order && @order.fulfillments.present? && @order.is_order_owner?(token)
+    if @order && @order.fulfillments.present?
       @success = true
       @fulfillment = @order.fulfillments.first
       if @fulfillment.present?
@@ -16,7 +16,7 @@ class TrackingInformationsController < ActionController::Base
         @tracking_information.reload
         @tracking_information = @tracking_information.map{|info| TrackingInformationQuery.single(info, @order) }
         @products = @order.line_items.map{|item| {image: ProductService.find_image(item.sku), quantity: item.quantity, item_name: item.name}}
-        @orders_history = Order.where(email: @order.email).where.not(id: @order.id)
+        @orders_history = Order.where(email: @order.email).where.not(id: @order.id) if token && @order.is_order_owner?(token)
       end
     else
       @success = false
