297faafa66ca928f35bb78c22ee3bb48ff8fbd62
Huy Tran <huytp.uit@gmail.com>
2018-04-14 11:57:20 +0700

    Generate tracking info

--- app/controllers/tracking_informations_controller.rb
+++ app/controllers/tracking_informations_controller.rb
@@ -7,6 +7,20 @@ class TrackingInformationsController < ActionController::Base
       @fulfillment = @order.fulfillments.first
       if @fulfillment.present?
         @tracking_information = @fulfillment.tracking_informations
+        if @tracking_information.none?
+          time_fulfilled = @fulfillment&.created_at
+          miskre_noti = { "tag" => "Submitted", "message" => "Electronic Notification Received", "location" => "Merchant", "checkpoint_time" => (time_fulfilled - 9.minutes).to_s}
+          miskre_submit = { "tag" => "Submitted", "message" => "Order Submitted", "location" => "Merchant", "checkpoint_time" => (time_fulfilled + 2.days + 15.minutes).to_s}
+          miskre_process = { "tag" => "Submitted", "message" => "Order Processed", "location" => "Merchant", "checkpoint_time" => (time_fulfilled + 3.days).to_s}
+          miskre_ship = { "tag" => "Submitted", "message" => "Order Shipped", "location" => "Merchant", "checkpoint_time" => (time_fulfilled + 5.days + 11.minutes).to_s}
+          tracking_history = [miskre_noti, miskre_submit, miskre_process, miskre_ship]
+          tracking = TrackingInformation.find_or_initialize_by(
+                        fulfillment_id: @fulfillment.id,
+                        tracking_number: @fulfillment.tracking_number
+                      )
+          tracking.tracking_history = tracking_history
+          tracking.save!
+        end
         if @order.tracking_number_real.present?
           unless @order.tracking_number_real == "none"
             TrackingInformationService.fetch_tracking_information @tracking_information.first
